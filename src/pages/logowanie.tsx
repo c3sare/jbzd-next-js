5740b8d86dcc4fca7a6d86de976b9c27fdf0078c
C3sare <marcinm222@gmail.com>
2023-06-02 15:30:19 +0200

    if logged then redirect to home page

--- src/pages/logowanie.tsx
+++ src/pages/logowanie.tsx
@@ -1,5 +1,6 @@
 import LoginPanel from "@/components/LoginPanel";
 import Seo from "@/components/Seo";
+import { withSessionSSR } from "@/lib/AuthSession/session";
 
 const Login = () => {
   return (
@@ -12,3 +13,16 @@ const Login = () => {
 };
 
 export default Login;
+
+export const getServerSideProps = withSessionSSR(
+  async function getServerSideProps({ req }): Promise<any> {
+    if (req.session.user?.logged || req.session.user?.login)
+      return {
+        redirect: "/",
+      };
+
+    return {
+      props: {},
+    };
+  }
+);
